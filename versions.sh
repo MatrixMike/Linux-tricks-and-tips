08a77704d071a8cafdae546461d022a240c09347
MikeH <matroxmike@icloud.com>
2018-01-27 23:04:57 +1100

    check arg 1

--- versions.sh
+++ versions.sh
@@ -1,7 +1,11 @@
 
 geany --version
+gnome-keyring version
+gtk_colours --version
 gedit --version
 firefox --version
 gnome-system-log --version
 gnome-system-monitor --version
+gcc --version
+gtkterm --help
 
