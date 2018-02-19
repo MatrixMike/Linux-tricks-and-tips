788a6cf7dc829b1264d89903f89419c5d910e09a
Mr M J H <matroxmike@icloud.com>
2018-02-19 18:50:11 +1100

    scp, java and keyring stuff

---
+++ java8Install.sh
@@ -0,0 +1,8 @@
+javac -version
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update 
+sudo apt-get install oracle-java8-installer 
+javac -version
+pwd
+sudo apt-get install oracle-java8-set-default
+
