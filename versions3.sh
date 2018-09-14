cf1b2193f5a32eba88e1cef02be93719a0ee0b77
Mike <matroxmike@icloud.com>
2018-09-14 15:13:32 +1000

    after some checking on alt systems

--- versions3.sh
+++ versions3.sh
@@ -6,10 +6,12 @@ echo "   aclocal:    $(aclocal --version | head -1)"
 echo "   autoconf:   $(autoconf --version | head -1)"
 echo "   autoheader: $(autoheader --version | head -1)"
 echo "   automake:   $(automake --version | head -1)"
-echo "   libtoolize: $($LIBTOOLIZE --version | head -1)"
+echo "   libtoolize: $(libtoolize --version | head -1)"
+# was $LIBTOOLIZE
 echo "   bison:      $(bison --version | head -1)"
 
 
-echo "   hexdump:      $(hexdump --version | head -1)"
+#echo "   hexdump:      $(hexdump --version | head -1)"
+# hexdump does not provide version info 
 echo "   cabextract:      $(cabextract --version | head -1)"
 
